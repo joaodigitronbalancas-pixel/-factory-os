a05032dd3f896ad827947f936199c41b1aa5e627
joaodigitronbalancas-pixel <joao.digitronbalancas@gmail.com>
2026-06-17 10:12:35 -0300

    Atualização geral projeto

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -12,10 +12,10 @@ export default function Login() {
     e.preventDefault();
 
     try {
-      const response = await api.post("/login", {
-        email,
-        senha,
-      });
+      const response = await api.post("/auth/login", {
+      email,
+      senha,
+});
 
       localStorage.setItem("token", response.data.token);
 
